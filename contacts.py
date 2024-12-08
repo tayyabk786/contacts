4db4b2ac8b7f5c10d1b12b51a03761eca8bef463
tayyabk786 <22310780@bucks.ac.uk>
2024-12-08 19:51:02 +0000

    Update contacts.py
    completing the hash table

--- contacts.py
+++ contacts.py
@@ -1,4 +1,4 @@
-import tkinter as tk
+import tkinter as tk #tkinter for the GUI
 from tkinter import messagebox, simpledialog
 
 
@@ -9,11 +9,11 @@ class HashTable:
         self.size = size  # the size for the hash table (is fixed)
         self.table = [[] for _ in range(self.size)]  # List of buckets
 
-    def _hash(self, key):
+    def hash(self, key):
         return hash(key) % self.size
 
     def add(self, key, value):
-        index = self._hash(key)
+        index = self.hash(key)
         bucket = self.table[index]
 
         # this is to see if the key already exists
@@ -24,3 +24,33 @@ class HashTable:
 
         # If key doesn't exist, then add it to the bucket
         bucket.append([key, value])
+    
+    def get(self, key):
+        index = self.hash(key)
+        bucket = self.table[index]
+
+        # Search the bucket for the key
+        for pair in bucket:
+            if pair[0] == key:
+                return pair[1]
+
+        return None  # Key not found
+
+    def delete(self, key):
+        index = self._hash(key)
+        bucket = self.table[index]
+
+        # search bucket for the key and remove it
+        for pair in bucket:
+            if pair[0] == key:
+                bucket.remove(pair)
+                return True
+
+        return False  # the key isnt found
+
+    def items(self):
+        for bucket in self.table:
+            for pair in bucket:
+                yield pair[0], pair[1]
+
+
